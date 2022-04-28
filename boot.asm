e5cc1e7aff544ca033724d6a8af43ff3ed43db94
Atul Patil <atuluttampatil.1757@gmail.com>
2022-04-28 10:44:07 +0530

    printing alphabets on screens

--- boot.asm
+++ boot.asm
@@ -1,4 +1,30 @@
+; hello there
+
+; switching to teltype mode to print letters;
+mov ah,0x0e
+mov al, 'H'
+
+; system interupt
+int 0x10
+mov ah,0x0e
+mov al, 'e'
+
+int 0x10
+mov ah,0x0e
+mov al, 'l'
+
+int 0x10
+mov ah,0x0e
+mov al, 'l'
+
+int 0x10
+mov ah,0x0e
+mov al, 'o'
+
+int 0x10
 jmp $
 
+
+; padding for the rest of the code to fill up 512 bytes of boot sector.
 times 510-($-$$) db 0
 db 0x55, 0xaa
